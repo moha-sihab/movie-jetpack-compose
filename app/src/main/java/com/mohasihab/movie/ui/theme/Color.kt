80d9a2fff58af174607a35302a272b1f1acdd3cc
moha-sihab <moha.sihab@gmail.com>
2024-02-14 16:13:43 +0700

    setup theming

--- app/src/main/java/com/mohasihab/movie/ui/theme/Color.kt
+++ app/src/main/java/com/mohasihab/movie/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.mohasihab.movie.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xffffb1c8)
+val OnPrimary = Color(0xff650033)
+val Secondary = Color(0xFF7dd0ff)
+val OnSecondary = Color(0xFF00344a)
+val Tertiary = Color(0xFF4fd8eb)
+val OnTertiary = Color(0xFF00363d)
+val Background = Color(0xFF201a1b)
+val OnBackground = Color(0xFFebe0e1)
+val Surface = Color(0xFF201a1b)
+val OnSurface = Color(0xFFebe0e1)
